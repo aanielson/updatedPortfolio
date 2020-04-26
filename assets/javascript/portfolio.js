c65eac4c02efa9864e364e8e6b12dfb4f95850ac
Abigail Nielson <aa.nielson18@gmail.com>
2020-04-26 15:24:16 -0700

    Adjusted styling so that the background image is lighter and the footer is green. Added another line to the bio page.

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrolltop > 300 || document.documentElement.scrollTop > 300) {
+  if (document.body.scrolltop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navlinks").style.height = "50px";
-    document.getElementById("navlinks").style.backgroundColor = rgb(242, 174, 2);
+    document.getElementById("navlinks").style.backgroundColor = "rgb(242, 174, 2)";
   } else {
     document.getElementById("navlinks").style.backgroundColor = "transparent";
   }
