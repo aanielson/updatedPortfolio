9a63bfdf0b94bb6a2738389a15a357c4ba84f158
Abigail Nielson <aa.nielson18@gmail.com>
2020-04-25 17:28:07 -0700

    input and styled navbar

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrolltop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("navlinks").style.height = "50px";
+    document.getElementById("navlinks").style.backgroundColor = "lightgray";
+  } else {
+    document.getElementById("navlinks").style.backgroundColor = "transparent";
+  }
+}
